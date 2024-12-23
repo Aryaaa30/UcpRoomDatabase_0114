7a210de523308937309350ceefbe91756e21fcec
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-12-23 09:10:23 +0700

    Menambahkan data class DosenEvent dan data class DosenUIState

--- app/src/main/java/com/example/session11_ucp2/ui/viewmodel/dosen/CreateDosenViewModel.kt
+++ app/src/main/java/com/example/session11_ucp2/ui/viewmodel/dosen/CreateDosenViewModel.kt
@@ -1,10 +1,30 @@
 package com.example.session11_ucp2.ui.viewmodel.dosen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.session11_ucp2.repository.RepositoryDosen
 
 class DosenViewModel(private val repositoryDosen: RepositoryDosen) : ViewModel(){
+    var uiState by mutableStateOf(DosenUIState())
 
+    fun updateState(dosenEvent: DosenEvent) {
+        uiState = uiState.copy(
+            dosenEvent = dosenEvent
+        )
+    }
 
 }
 
+data class DosenUIState(
+    val dosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorStateDosen = FormErrorStateDosen(),
+    val snackBarMessage: String? = null
+)
+
+data class DosenEvent(
+    val nidn: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = ""
+)
